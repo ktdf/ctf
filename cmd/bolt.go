0f68941f6adb324debcfa39fd9b8448d51636198
Lev Valuev <lev.valuev@gmail.com>
2021-06-26 17:59:55 +0300

    Boltdb. Function generates buckets

--- cmd/bolt.go
+++ cmd/bolt.go
@@ -1,1 +1,25 @@
 package cmd
+
+import (
+	"github.com/boltdb/bolt"
+	"time"
+)
+
+type conn struct {
+	db *bolt.DB
+}
+
+func (db *conn) Generate() error {
+	if err := db.db.Batch(func(tx *bolt.Tx) error {
+		for _, name := range []string{"active", "completed"} {
+			_, err := tx.CreateBucketIfNotExists([]byte(name))
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+	return nil
+}
