b8eb62171bfd8e3015614dc2e89d90a334480c72
Lev Valuev <lev.valuev@gmail.com>
2021-06-26 20:28:20 +0300

    Now we can remove tasks

--- cmd/rm.go
+++ cmd/rm.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/boltdb/bolt"
 	"github.com/spf13/cobra"
+	"strconv"
 )
 
 func init() {
@@ -13,6 +15,24 @@ var rmCmd = &cobra.Command{
 	Use: "rm",
 	Short: "Removes task from the list",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("rm")
+		if len(args) != 1 {
+			fmt.Println("Only one integer argument should be provided.")
+			return
+		}
+		var con conn
+		db , err := bolt.Open(DbFile, 0755, nil)
+		checkPanic(err)
+		con.db = db
+		err = con.Generate()
+		checkPanic(err)
+		i, err := strconv.Atoi(args[0])
+		checkPanic(err)
+		ret, err := con.RmTask(i)
+		checkPanic(err)
+		if ret == "" {
+			fmt.Println("There is no such task")
+		} else {
+			fmt.Println("\"" + ret + "\" has been deleted")
+		}
 	},
 }
