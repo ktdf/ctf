4fd81af987ae32c63be5e8d18c4cf93be29beb40
Lev Valuev <lev.valuev@gmail.com>
2021-06-26 19:12:12 +0300

    Now we can mark tasks as done

--- cmd/do.go
+++ cmd/do.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/boltdb/bolt"
 	"github.com/spf13/cobra"
+	"strconv"
 )
 
 func init() {
@@ -13,6 +15,20 @@ var addDo = &cobra.Command{
 	Use: "do",
 	Short: "Marks task as complete",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Do")
+		if len(args) != 1 {
+			fmt.Println("Only one integer argument should be provided.")
+			return
+		}
+		var con conn
+		db , err := bolt.Open(DbFile, 0755, nil)
+		checkPanic(err)
+		con.db = db
+		err = con.Generate()
+		checkPanic(err)
+		i, err := strconv.Atoi(args[0])
+		checkPanic(err)
+		ret, err := con.DoTask(i)
+		checkPanic(err)
+		fmt.Println("\""+ret+"\" has been completed")
 	},
 }
