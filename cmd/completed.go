a1b2df590ecede39f13db4a7849b3366d2af795e
Lev Valuev <lev.valuev@gmail.com>
2021-06-26 19:47:11 +0300

    Now we can see completed tasks

--- cmd/completed.go
+++ cmd/completed.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/boltdb/bolt"
 	"github.com/spf13/cobra"
 )
 
@@ -10,9 +11,18 @@ func init() {
 }
 
 var completedCmd = &cobra.Command{
-	Use: "completed",
+	Use:   "completed",
 	Short: "List completed tasks",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("completed")
+		var con conn
+		db, err := bolt.Open(DbFile, 0755, nil)
+		checkPanic(err)
+		con.db = db
+		err = con.Generate()
+		checkPanic(err)
+		tasks := con.ListCompletedTasks()
+		for n, task := range tasks {
+			fmt.Printf("%3v: %s\n", n+1, task)
+		}
 	},
 }
