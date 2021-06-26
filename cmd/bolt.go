4250c588df6f90e8f6ca4eed315d678a42f559a6
Lev Valuev <lev.valuev@gmail.com>
2021-06-26 18:00:49 +0300

    Boltdb. Function creates new records in the database

--- cmd/bolt.go
+++ cmd/bolt.go
@@ -23,3 +23,18 @@ func (db *conn) Generate() error {
 	}
 	return nil
 }
+
+func (db *conn) AddTask(task []string) error {
+	timestamp := TimestampToByte(time.Now().Unix())
+	if err := db.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("active"))
+		err := b.Put(timestamp, []byte(sliceToString(task)))
+		if err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+	return nil
+}
