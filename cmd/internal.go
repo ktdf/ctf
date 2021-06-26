4085ce36452b5068493bc485bfa78bfad2911a05
Lev Valuev <lev.valuev@gmail.com>
2021-06-26 17:38:45 +0300

    slice of strings to strings converter added

--- cmd/internal.go
+++ cmd/internal.go
@@ -1,7 +1,17 @@
 package cmd
 
+import "strings"
+
 func checkPanic(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func sliceToString(s []string) string {
+	var db strings.Builder
+	for _, word := range s {
+		db.WriteString(word)
+	}
+	return db.String()
 }
