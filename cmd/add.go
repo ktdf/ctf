6c88fc2ee0c015d63ecd811033426c1e9ec8992f
Lev Valuev <lev.valuev@gmail.com>
2021-06-26 18:01:49 +0300

    'add' keyword created

--- cmd/add.go
+++ cmd/add.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/boltdb/bolt"
 	"github.com/spf13/cobra"
 )
 
@@ -13,6 +14,14 @@ var addCmd = &cobra.Command{
 	Use: "add",
 	Short: "Adds new task to the list",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("List")
+		var con conn
+		db , err := bolt.Open("ctm.db", 0755, nil)
+		checkPanic(err)
+		con.db = db
+		err = con.Generate()
+		checkPanic(err)
+		err = con.AddTask(args)
+		checkPanic(err)
+		fmt.Println("\""+sliceToString(args)+"\" has been added to the task list")
 	},
 }
